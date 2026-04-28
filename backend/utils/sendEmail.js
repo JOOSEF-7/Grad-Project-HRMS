a7965c5b0d150ee311a84b85278dcd8e371745ee
JOOSEF-7 <jooshetaia160@gmail.com>
2026-04-28 11:26:34 +0300

    force nodemailer to use IPv6 ENETUNREACH block

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,13 +3,13 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
+        requireTLS: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
         },
-        family: 4,
     });
 
     const mailOptions = {
